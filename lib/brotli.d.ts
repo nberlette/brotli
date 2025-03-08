3a089d338566fa5208653662b27f661abbf41a82
Nicholas Berlette <nick@berlette.com>
2025-03-08 13:26:50 +0000

    chore: update wasm build

--- lib/brotli.d.ts
+++ lib/brotli.d.ts
@@ -3,8 +3,11 @@
 // deno-fmt-ignore-file
 
 /**
- * Decompresses a Brotli-compressed byte array, returning the decompressed
- * data as a `Vec<u8>`. If an error occurs during decompression, a `JsValue`
- * containing the error message is returned instead.
+ * Decompresses a Brotli-compressed buffer, returning the decompressed data.
+ *
+ * If the data cannot be decompressed, a copy of the input is returned.
+ *
+ * @param {Uint8Array} data - The Brotli-compressed data to decompress.
+ * @returns {Uint8Array} - The decompressed data.
  */
 export function decompress(data: Uint8Array): Uint8Array;
